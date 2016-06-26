70cba7950d038f59bbccc69ffe007cfe5a2c2b11
Kali <test@ya.ru>
2016-06-27 00:18:08 +0400

    Initial commit

---
+++ src/main/java/com/test/controller/MockController.java
@@ -0,0 +1,20 @@
+package com.test.controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by SERGE on 06.07.2014.
+ */
+public class MockController extends HttpServlet{
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.getWriter().write("Hello World");
+
+    }
+}
---
+++ src/main/webapp/product.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Matrix
+  Date: 07.07.2014
+  Time: 16:19
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <b>Product Page</b>
+    <br />id: ${product.id}
+    <br />name: ${product.name}
+    <br /><a href="/eshop/index.jsp">Main Page</a>
+</body>
+</html>
